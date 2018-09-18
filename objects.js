88c6f7e119395eacc66f916029cb103077768907
caboky31 Bernard <caboky31@hotmail.com>
2018-09-18 20:49:46 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {
-Foo Fighters: "Everlong",
+'Foo Fighters': "Everlong",
 Prince: "Purle Rain"
 };
 
@@ -8,10 +8,10 @@ function updatePlaylist(playlist, artist, song) {
   playlist[artist] = song
   
   return playlist
-};
+}
 
 function removeFromPlaylist(playlist, artist){
   delete playlist[artist]
   
   return playlist
-};
+}
